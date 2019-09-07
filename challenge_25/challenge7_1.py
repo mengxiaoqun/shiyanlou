741a42acd664537ac1e4e7dbdce351aa61fabba6
mengxiaoqun <467023440@qq.com>
2019-09-07 10:44:46 +0800

    25

--- challenge_25/challenge7_1.py
+++ challenge_25/challenge7_1.py
@@ -3,24 +3,32 @@
 import pandas as pd
 
 def co2():
-    c1 = pd.read_excel('ClimateChange.xlsx',sheetname='Data')
-    c2 = c1[c1['Series code'] == 'EN.ATM.CO2E.KT'].set_index('Country code')
-    c2.drop(c2.columns[[0,1,2,3,4]],axis=1,inplace=True)
-    c2.replace({'..':pd.np.NaN},inplace=True)
-    c3 = c2.fillna(method='ffill',axis=1).fillna(method='bfill',axis=1).dropna()
-    c3['Sum emissions'] = c3.sum(axis=1)
-    c3.drop(c3.columns[[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]],axis=1,inplace=True)
+
+#1读取data表。筛选数据并设置index索引。删除多余行。替换无效数据为缺失值
+#自动填充缺失值并删除整行都为空的数据。
+
+    df_data = pd.read_excel('ClimateChange.xlsx',sheetname='Data')
+    df_data = df_data[df_data['Series code'] == 'EN.ATM.CO2E.KT'].set_index('Country code')
+    df_data.drop(df_data.columns[[0,1,2,3,4]],axis=1,inplace=True)
+    df_data.replace({'..':pd.np.NaN},inplace=True)
+    df_data = df_data.fillna(method='ffill',axis=1).fillna(method='bfill',axis=1)
+    df_data.dropna(how='all',inplace=True)
+    df_data['Sum emissions'] = df_data.sum(axis=1)
+    df_data = df_data['Sum emissions']
+   
     
-    c4 = pd.read_excel('ClimateChange.xlsx',sheetname='Country').set_index('Country code')
-    c4.drop(c4.columns[[1,2,4]],axis=1,inplace=True)
-    c5 = pd.concat([c3,c4],axis=1).dropna()
+    df_country = pd.read_excel('ClimateChange.xlsx',sheetname='Country').set_index('Country code')
+    df_country.drop(df_country.columns[[1,2,4]],axis=1,inplace=True)
     
-    r1 = c5[['Sum emissions','Income group']].groupby('Income group').sum()
-    r2 = c5.sort_values('Sum emissions',ascending=False).groupby('Income group',as_index=False).first().set_index('Income group')
-    r2.columns = ['Highest emissions','Highest emission country']
-    r3 = c5.sort_values('Sum emissions',ascending=True).groupby('Income group',as_index=False).first().set_index('Income group')
-    r3.columns = ['Lowest emission','Lowest emission country']
-    results = pd.concat([r1,r2,r3],axis=1)
+
+    df_climate = pd.concat([df_data,df_country],axis=1,join='inner')
+   
+    sum_climate = df_climate[['Sum emissions','Income group']].groupby('Income group').sum()
+    highest_climate = df_climate.sort_values('Sum emissions',ascending=False).groupby('Income group',as_index=False).first().set_index('Income group')
+    highest_climate.columns = ['Highest emissions','Highest emission country']
+    lowest_climate = df_climate.sort_values('Sum emissions',ascending=True).groupby('Income group',as_index=False).first().set_index('Income group')
+    lowest_climate.columns = ['Lowest emissions','Lowest emission country']
+    results = pd.concat([sum_climate,highest_climate,lowest_climate],axis=1)
     print(results)
     return results
 
